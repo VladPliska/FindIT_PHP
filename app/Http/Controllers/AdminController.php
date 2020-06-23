93bdd8cdb1cf22a2b9b985fd0f22ac77e7ad29a3
vlad.pliska <vlad@ceek.com>
2020-06-23 22:49:36 +0300

    h

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -71,7 +71,7 @@ class AdminController extends Controller
                             'delete' => true
                         ]);
                     } catch (\Exception $e) {
-                        dd($e);
+//                        dd($e);
                         return response()->json([
                             'delete' => false
                         ]);
@@ -87,7 +87,7 @@ class AdminController extends Controller
                             'delete' => true
                         ]);
                     } catch (\Exception $e) {
-                        dd($e);
+//                        dd($e);
                         return response()->json([
                             'delete' => false
                         ]);
